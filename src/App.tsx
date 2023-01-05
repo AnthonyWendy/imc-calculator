1b03b4f5aaf163a72a234241a7476cc594e2fd38
AnthonyWendy <anthonywendyantunesdelima@gmail.com>
2023-01-04 21:34:14 -0300

    Criado a base dos componentes do lado esquerdo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.css';
+import poweredImage from './assets/powered.png';
 
-function App() {
+const App = () => {
+
+  const [ heigthField, setHeigthField ] = useState<number>(0); //useState<number>(0) = não podera setar outro tipo de argumento, somente number.
+  const [weightFiled, setWeightFiled] = useState<number>(0);
+  
+  const hancleCalculateButton = () => {
+    if(heigthField && weightFiled){
+
+    } else {
+      alert("Ainda falta informações....")
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="" width={150} />
+        </div>
       </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu IMC</h1>
+          <p>Criado no século 19 pelo matemático Lambert Quételet, o Índice de Massa Corporal, conhecido pela sigla IMC, é um cálculo simples que permite medir se alguém está ou não com o peso ideal. Ele aponta se o peso está adequado ou se está abaixo ou acima do peso.</p>
+
+
+          <input
+            type="number"
+            placeholder="Digite a sua altura. Ex: 1.5 mts"
+            value={heigthField > 0 ? heigthField : ''}
+            onChange={e => setHeigthField(parseFloat(e.target.value))}
+          />
+
+          <input
+            type="number"
+            placeholder="Digite o seu peso. Ex: 60.5 kg"
+            value={weightFiled > 0 ? weightFiled : ''}
+            onChange={e => setWeightFiled(parseFloat(e.target.value))}
+          />
+
+          <button onClick={hancleCalculateButton}>Calcular</button>
+
+
+        </div>
+        <div className={styles.rightSide}>
+          ..
+        </div>
+      </div>
+
     </div>
   );
 }
